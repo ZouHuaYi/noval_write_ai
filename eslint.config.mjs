b4e7b63b9a796e3533d49357cb83b19ab48288d7
zouhy <1004753639@qq.com>
2026-02-05 17:48:18 +0800

    fix: 修改eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+﻿// ESLint 扁平配置，适配 Vue3 + TS
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import vueParser from 'vue-eslint-parser'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-unused-vars': 'off',
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx,js,cjs,mjs}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+]
---
+++ prettier.config.cjs
@@ -0,0 +1,5 @@
+﻿// Prettier 配置：单引号且不使用分号
+module.exports = {
+  semi: false,
+  singleQuote: true,
+}
